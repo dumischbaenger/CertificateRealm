5e2332c0b21a8f5d08c4e649b2712d6c58109003
Bernhard Donaubauer <bdonauba@gmail.com>
2024-02-22 20:02:06 +0100

    initial commit

---
+++ src/main/java/net/ensode/glassfishbook/LogoutServlet.java
@@ -0,0 +1,21 @@
+package net.ensode.glassfishbook;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class LogoutServlet extends HttpServlet
+{
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException
+  {
+    request.getSession().invalidate();
+
+    response.sendRedirect("index.jsp");
+  }
+}
